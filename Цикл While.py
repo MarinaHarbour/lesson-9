dd8a5ae25809d64e725fd4ab8656d4ba9818d531
MarinaHarbour <shunatovamarina@gmail.com>
2024-08-21 20:04:33 +0200

    Add files via upload

--- Цикл While.py
+++ Цикл While.py
@@ -1,11 +1,13 @@
-my_list = [42, 69, 322, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 while index < len(my_list):
     element = my_list[index]
+    index += 1
     if element < 0:
         break
+    if element == 0:
+        continue
     print(element)
-    index += 1
 
 
 
